1ff233c86629ea98ce8c7390120a61b3c9ce3b67
MuhammadSahimBhaur <21100317@lums.edu.pk>
2023-03-15 10:39:53 +0500

    removed unused main function

--- backend-server/app.js
+++ backend-server/app.js
@@ -10,12 +10,6 @@ const PASSWORD = process.env.PASSWORD;
 
 mongoose.set('strictQuery', false);
 
-
-async function main() {
-  mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.e0s0zo8.mongodb.net/data?retryWrites=true&w=majority`);
-}
-
-
 export async function register(email, password) {
   mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.e0s0zo8.mongodb.net/data?retryWrites=true&w=majority`);
   const newUser = new userModel({ email, password, tweets: [] });
