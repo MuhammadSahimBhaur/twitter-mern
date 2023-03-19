b0a92b7ce840b8306a846085cf68f845a7e1d657
MuhammadSahimBhaur <21100317@lums.edu.pk>
2023-03-19 17:14:22 +0500

    Used Card component in register comp direclty

--- client/twitter-client/components/Register.tsx
+++ client/twitter-client/components/Register.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "src/styles/Register.module.css";
 import { useAuth } from "@/hooks/useAuth";
+import Card from "components/Card";
 
 const Register: React.FC = () => {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
@@ -18,45 +19,39 @@ const Register: React.FC = () => {
 
   return (
     <div>
-      <div className={styles.register}>
-        <form>
-          <label htmlFor="email">Email</label>
-          <br />
-          <input
-            type="text"
-            id="email"
-            name="email"
-            value={credentials.email}
-            onChange={handleChange}
-          />
-          <br />
-          <label htmlFor="password">Password</label>
-          <br />
-          <input
-            type="text"
-            id="password"
-            name="password"
-            value={credentials.password}
-            onChange={handleChange}
-          />
-          <button
-            className="rounded-full"
-            type="button"
-            onClick={handleSubmit}
-            name="register"
-          >
-            Register
-          </button>
-          <button
-            className="rounded-full"
-            type="button"
-            onClick={handleSubmit}
-            name="login"
-          >
-            Login
-          </button>
-        </form>
-      </div>
+      <Card width={150} height={120}>
+        <div className={styles.register}>
+          <form>
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={credentials.email}
+              onChange={handleChange}
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              type="text"
+              id="password"
+              name="password"
+              value={credentials.password}
+              onChange={handleChange}
+            />
+            <button
+              className="rounded-full"
+              type="button"
+              onClick={handleSubmit}
+              name="register"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </Card>
     </div>
   );
 };
