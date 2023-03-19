ee653cbb6e0349ceb871457074c2078263484c2b
MuhammadSahimBhaur <21100317@lums.edu.pk>
2023-03-19 17:10:01 +0500

    moved logic to "Routes" folder

--- backend-server/server.js
+++ backend-server/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { mongoConnect } from "./app.js";
-import { register, isDuplicate, login } from "./Controllers/authentication.js";
+import AuthRouter from './Routes/authRoute.js';
+import tweetsRouter from "./Routes/tweetsRoute.js";
+
 
 
 const app = express()
 app.use(express.json()) // for parsing application/json
 
-mongoConnect().then(console.log("Connected to mongodb")); // connecting to the mongoDB
+mongoConnect().then((res) => { res ? console.log("Connected to mongodb") : console.log("Couldn't connect") }); // connecting to the mongoDB
 
 const port = 4000
 
@@ -14,26 +16,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/api/register', async (req, res) => {
-    const { email, password } = req.body
-
-    // need to add logic to check for duplicate emails
-    if (await isDuplicate(email)) {
-        res.send("Email already exists")
-    } else {
-        register(email, password).then((mongoRes) => {
-            res.json(mongoRes)
-        })
-    }
-
-});
-
-app.post('/api/login', async (req, res) => {
-    const { email, password } = req.body
-    login(email, password).then((mongoRes) => {
-        res.json(mongoRes)
-    })
-});
+app.use("/api/auth/", AuthRouter)
+app.use("/api/tweet/", tweetsRouter)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
