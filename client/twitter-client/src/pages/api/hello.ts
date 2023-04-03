8c12ebc45006245edb67b364ee21b14a6131830c
MuhammadSahimBhaur <64275858+MuhammadSahimBhaur@users.noreply.github.com>
2023-04-03 15:13:06 +0500

    commented out all code

--- client/twitter-client/src/pages/api/hello.ts
+++ client/twitter-client/src/pages/api/hello.ts
@@ -12,6 +12,6 @@
 //   res.status(200).json({ name: 'John Doe' })
 // }
 
-export default function handler(req, res: any) {
-  res.status(200).json({ name: "John Doe" });
-}
+// export default function handler(req, res: any) {
+//   res.status(200).json({ name: "John Doe" });
+// }
